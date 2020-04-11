314255eb774d06e82e56b2d9d2bd4de3b999da52
Anthony Heremans <anthony_heremans@hotmail.com>
2020-04-11 22:59:09 +0200

    SYWA-5:Init word dump service, setup docker file

--- index.js
+++ index.js
@@ -4,5 +4,9 @@ const app = express();
 const port = process.env.PORT || "8080";
 
 app.get("/", (req, res) => {
-    res.status(200).send("Word Dump Service is up and running!");
+    res.status(200).send("Word dump Service is up and running!");
     });
+
+  app.listen(port, () => {
+    console.log(`Listening to requests on http://localhost:${port}`);
+  });
